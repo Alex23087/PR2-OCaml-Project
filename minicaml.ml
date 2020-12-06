ebe60da4a08870d6d51462ddf6f5ea8ec49673c7
Alessandro Scala <alessandro.scala8@gmail.com>
2020-12-06 22:00:45 +0100

    Started adding Set operations, Added equals and contains for evaluationTypes

--- minicaml.ml
+++ minicaml.ml
@@ -4,12 +4,20 @@ exception InvalidTypeDescriptor
 exception RuntimeException
 exception ApplyingNonFunctional
 exception IncorrectParameterListLength
+exception TypeException
+exception InterpreterException
 
 
 (*Types*)
 type identifier = string
 
 type 'a environment = identifier -> 'a
+
+type typeDescriptor =
+    | Integer
+    | Boolean
+    | String
+    | Set of typeDescriptor
 
 type expression =
     | IntImm of int
@@ -27,13 +35,16 @@ type expression =
     | Let of identifier * expression * expression
     | Func of identifier list * expression
     | Apply of expression * expression list
-
-
-type typeDescriptor =
-    | Integer
-    | Boolean
-    | String
-    | Set of typeDescriptor
+    | EmptySet of typeDescriptor
+    | SingletonSet of typeDescriptor * expression
+    | SetOf of typeDescriptor * expression list
+    | SetPut of expression * expression
+    | SetRemove of expression * expression
+    | SetIsEmpty of expression
+    | SetContains of expression * expression
+    | SetIsSubset of expression * expression
+    | SetMin of expression
+    | SetMax of expression
 
 type evaluationType =
     | Int of int
@@ -72,13 +83,14 @@ let rec checkType (evType: evaluationType) (typeDesc: typeDescriptor) = match ty
         | Str(_) -> true
         | _ -> false)
     | Set(tDesc) -> (match evType with
-        | SetT(tDesc, elList) -> (let rec checkSet (elements: evaluationType list) =
-            match elements with
-                | [] -> true
-                | el::els -> (checkType el tDesc) && (checkSet els)
-            in checkSet elList)
+        | SetT(tDesc, elList) -> checkTypeMultiple tDesc elList
         | _ -> false)
     (*| _ -> raise InvalidTypeDescriptor*)
+
+and checkTypeMultiple (tDesc: typeDescriptor) (elements: evaluationType list) =
+    match elements with
+        | [] -> true
+        | el::els -> (checkType el tDesc) && (checkTypeMultiple tDesc els)
 
 let rec getTypeDescriptor (evType: evaluationType) = match evType with
     | Int(_) -> Integer
@@ -97,14 +109,38 @@ let rec printTypeDescriptor (tDesc: typeDescriptor) = match tDesc with
     | String -> print_string "String"
     | Set(x) -> print_string "Set(" ; printTypeDescriptor x ; print_string ")"
 
-let printEvaluationType (evType: evaluationType) = match evType with
+let rec printEvaluationType (evType: evaluationType) = match evType with
     | Int(x) -> print_int x
     | Bool(x) -> print_boolean x
     | Str(x) -> print_string x
-    | SetT(x, _) -> print_string "SetT(" ; printTypeDescriptor x ; print_string ")"
-    | Closure(_,_,_) -> print_string "Closure"
+    | SetT(_, x) -> print_string "SetT(" ; (let rec p l =
+            match l with
+                | [] -> print_string ""
+                | x::[] -> printEvaluationType x
+                | x::xs -> printEvaluationType x ; print_string ", " ; p xs
+            in p x
+        ) ; print_string ")"
+    | Closure(id,body,_) -> print_string "Closure(" ;
+        (let rec pID l = match l with
+            | [] -> print_string ""
+            | x::[] -> print_string x
+            | x::xs -> print_string x ; print_string ", " ; pID xs
+        in pID id);
+        print_string ") <";
+        print_string "expr";
+        print_string ">"
     (*| Unbound -> print_string "Unbound"*)
 
+(*Utility functions*)
+let rec evtEquals (elem1: evaluationType) (elem2: evaluationType) = match (elem1, elem2) with
+    | Int(a), Int(b) -> a=b
+    | Bool(a), Bool(b) -> a=b
+    | Str(a), Str(b) -> a=b
+    | SetT(_,_), SetT(_,_) -> raise InterpreterException (*TODO Add set equality function*)
+
+and contains (lst: evaluationType list) (elem: evaluationType) = match lst with
+    | [] -> false
+    | x::xs -> if (evtEquals x elem) then true else (contains xs elem)
 
 (*Builtins*)
 (*let binaryOperator (a: identifier) (b: identifier) (aT: typeDescriptor) (bT: typeDescriptor) (f: )*)
@@ -136,6 +172,17 @@ let boolNot x = match (checkType x Boolean, x) with
     | _ -> raise RuntimeException
 
 
+let setPut set element = match set with
+    | SetT(td, elems) ->(
+        if (checkType element td) then
+            if (contains elems element) then
+                set
+            else
+                SetT(td, element::elems)
+        else
+            raise TypeException)
+    | _ -> raise TypeException
+
 (*Evaluation*)
 let rec eval (e: expression) (env: evaluationType environment) = match e with
     | IntImm(n) -> Int(n)
@@ -155,15 +202,36 @@ let rec eval (e: expression) (env: evaluationType environment) = match e with
             (match (checkType g Boolean, g) with
                 | (true, Bool(gev)) when gev -> eval b1 env
                 | (true, Bool(gev)) when not gev -> eval b2 env
-                | (_, _) -> raise RuntimeException
+                | (_, _) -> raise TypeException
             )
         )
     | Func(identifiers, body) -> Closure(identifiers, body, env)
-    | Apply(func, parameters) -> (match (eval func env) with
+    | Apply(func, parameters) -> (let f = (eval func env) in match f with
             | Closure(identifiers, body, fenv) ->
-                    eval body (bindList identifiers (List.map (fun x -> eval x env) parameters) fenv)
+                    eval body (bindList identifiers (List.map (fun x -> eval x env) parameters) (bind "rec" f fenv))
             | _ -> raise ApplyingNonFunctional
         )
+    | EmptySet(typeDesc) -> SetT(typeDesc, [])
+    | SingletonSet(typeDesc, expr) -> (let value = eval expr env in
+        if (checkType value typeDesc) then
+            SetT(typeDesc, [value])
+        else
+            raise TypeException)
+    | SetOf(typeDesc, exprs) -> (let vals = List.map (fun x -> eval x env) exprs in
+        if (checkTypeMultiple typeDesc vals) then
+            SetT(typeDesc, vals)
+        else
+            raise TypeException)
+    | SetPut(exp1, exp2) -> setPut (eval exp1 env) (eval exp2 env)
+(*
+| SetPut of expression * expression
+| SetRemove of expression * expression
+| SetIsEmpty of expression
+| SetContains of expression * expression
+| SetIsSubset of expression * expression
+| SetMin of expression
+| SetMax of expression
+*)
     (*| _ -> failwith ("not implemented yet")*)
 
 
@@ -181,6 +249,7 @@ let x = If(Or(BoolImm(false), BoolImm(false)), IntImm(10), IntImm(20))
 let v = printEvaluationType (eval x emptyEvaluationEnvironment)
 let v = print_string "\n"
 let x = Let("f", Func(["a"; "b"], Times(Den("a"), Den("b"))), Apply(Den("f"), [IntImm(10); IntImm(4)]))
+let x = Let("f", Func(["a"; "b"], Times(Den("a"), Den("b"))), Den("f"))
 let v = printEvaluationType (eval x emptyEvaluationEnvironment)
 let v = print_string "\n"
 
@@ -189,3 +258,28 @@ let v = printEvaluationType x
 let v = print_string "\n"
 
 let td = printTypeDescriptor (getTypeDescriptor ( Int 10 ))
+
+let x = eval (EmptySet(Integer)) emptyEvaluationEnvironment;;
+printEvaluationType x;
+print_string "\n";
+printTypeDescriptor (getTypeDescriptor x);
+print_string "\n"
+
+
+let x = eval (SingletonSet(Integer, IntImm 7 )) emptyEvaluationEnvironment;;
+printEvaluationType x;
+print_string "\n";
+printTypeDescriptor (getTypeDescriptor x);
+print_string "\n"
+
+let x = eval (SetOf(Integer, [IntImm 10; IntImm 20; Times(IntImm 5, IntImm 6)])) emptyEvaluationEnvironment;;
+printEvaluationType x;
+print_string "\n";
+printTypeDescriptor (getTypeDescriptor x);
+print_string "\n"
+
+let x = eval (SetPut(SetOf(Integer, [IntImm 10; IntImm 20; Times(IntImm 5, IntImm 6)]), IntImm(10))) emptyEvaluationEnvironment;;
+printEvaluationType x;
+print_string "\n";
+printTypeDescriptor (getTypeDescriptor x);
+print_string "\n"
